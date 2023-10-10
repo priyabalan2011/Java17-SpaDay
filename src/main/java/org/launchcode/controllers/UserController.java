be2f2b0b3a97fdf32235f5f8aaf2913f41ad9feb
priya <priyabalanusa@gmail.com>
2023-10-09 19:15:23 -0500

    first commit

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -10,23 +12,27 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+
+        model.addAttribute("user",new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
-            return "user/index";
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+        if(errors.hasErrors())
+        {
+            return "user/add";
         }
-        else {
+        else if (!user.getPassword().equals(verify)) {
+            model.addAttribute("verify", verify);
             model.addAttribute("error", "Passwords do not match");
             return "user/add";
         }
+       // model.addAttribute("user", user);
+        return "user/index";
+
+
 
     }
 
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,21 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message = "Username should be required")
+    @Size(min=5, max=15, message = "UserName must be between 5 and 15 characters")
+    @NotNull
     private String username;
+    //@NotBlank(message = "Email should be required")
+    @Email(message = "Invalid email. Try again.")
     private String email;
+    @NotBlank(message = "Password should be required")
+    @Size(min=6,message = "Password should be 6 character length")
+    @NotNull
     private String password;
 
     public User() {
